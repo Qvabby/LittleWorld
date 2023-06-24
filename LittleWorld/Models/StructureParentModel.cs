7688537e0a98cf03359e7189a211b1d27863708a
WYBM <saluqvadze2006@gmail.com>
2023-06-25 00:19:24 +0400

    Add project files.

---
+++ LittleWorld/Models/StructureParentModel.cs
@@ -0,0 +1,11 @@
+﻿namespace LittleWorld.Models
+{
+    public class StructureParentModel
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+        string Type { get; set; }
+        float Price { get; set; }
+        string ImageName { get; set; }
+    }
+}
